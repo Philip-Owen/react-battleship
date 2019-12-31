4d5953875df0a2e4b223b56d6a0cdd1f7ef144f8
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2019-12-30 20:29:32 -0600

    created basic game controls

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function Controls({ generateBoard }) {
+function Controls({ generateBoard, accepted, acceptPlacement, endGame }) {
 	return (
 		<div>
-			<button onClick={() => generateBoard(true)}>Generate Board</button>
+			{!accepted ? (
+				<>
+					<button onClick={() => generateBoard(true)}>Generate Board</button>
+					<button onClick={() => acceptPlacement()}>Accept Placement</button>
+				</>
+			) : null}
+
+			{accepted ? <button onClick={() => endGame()}>End Game</button> : ''}
 		</div>
 	);
 }
 
 const mapDispatchToProps = dispatch => ({
 	generateBoard: task => dispatch({ type: 'GENERATE_BOARD', payload: task }),
+	acceptPlacement: () => dispatch({ type: 'ACCEPT_SHIP_PLACEMENT', playload: null }),
+	endGame: () => dispatch({ type: 'END_GAME', playload: null }),
 });
 
-export default connect(null, mapDispatchToProps)(Controls);
+const mapStateToProps = state => ({
+	accepted: state.placementAccepted,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Controls);
--- src/reducers.js
+++ src/reducers.js
@@ -5,12 +5,18 @@ const initialState = {
 	computerBoard: placeShipsOnBoard(),
 	computerMoves: [],
 	gameLog: [],
+	placementAccepted: false,
 };
 
 function reducers(state = initialState, action) {
 	switch (action.type) {
 		case 'GENERATE_BOARD':
 			return { ...state, playerBoard: placeShipsOnBoard() };
+		case 'ACCEPT_SHIP_PLACEMENT':
+
+			return { ...state, placementAccepted: true };
+		case 'END_GAME':
+			return initialState;
 		default:
 			return state;
 	}
