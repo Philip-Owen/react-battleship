2674eef8e7c72d8415442c608cfb804cbf0bba49
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2020-01-03 17:56:18 -0600

    Updates

---
+++ precache-manifest.5a706ab7fa8973b6207905242339c44e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d297314f4e043aaf3899c17c67cf45fd",
+    "url": "/react-battleship/index.html"
+  },
+  {
+    "revision": "747f534b0827e2822e22",
+    "url": "/react-battleship/static/css/main.ea160cce.chunk.css"
+  },
+  {
+    "revision": "cef50869cc21043e9aa3",
+    "url": "/react-battleship/static/js/2.74a0e9f1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-battleship/static/js/2.74a0e9f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "747f534b0827e2822e22",
+    "url": "/react-battleship/static/js/main.9467b5a9.chunk.js"
+  },
+  {
+    "revision": "4ae0d0bd70c6084ee741",
+    "url": "/react-battleship/static/js/runtime-main.e2892791.js"
+  }
+]);
